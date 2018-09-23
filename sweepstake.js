c305c3ebfbfc215daab1fc58de194083a38f67bc
Joe Buckle <joe@white-fire.co.uk>
2018-09-23 17:00:32 +0100

    Code cleanup

--- sweepstake.js
+++ sweepstake.js
@@ -1,18 +1,8 @@
 let Sweepstake = function() {
     let $this = this;
 
-    // Setup HTML
-    $this.container = document.getElementById('sweepstake');
-    $this.containerParticipants = document.createElement('DIV');
-      $this.containerParticipants.setAttribute('id', 'containerParticipants');
-    $this.containerOutput = document.createElement('DIV');
-      $this.containerOutput.setAttribute('id', 'containerOutput');
-    $this.generateBtn = document.createElement('BUTTON');
-      $this.generateBtn.setAttribute('id', 'generateBtn');
-      $this.generateBtn.textContent = 'Generate output';
-
     // Setup data
-    $this.participants = [];
+    $this.participants = new Array;
     $this.contender1 = "Anthony Joshua";
     $this.contender2 = "Alexander Povetkin";
     $this.winBecause = [
@@ -23,7 +13,7 @@ let Sweepstake = function() {
       "Win on points",
       "Win due to disqualification"
     ];
-    $this.winOptions = [];
+    $this.winOptions = new Array;
 
     $this.setWinOptions = function() {
       let options = new Array;
@@ -32,7 +22,6 @@ let Sweepstake = function() {
         options.push('<b>' + $this.contender1 + '</b>' + ' ' + $this.winBecause[i]);
         options.push('<b>' + $this.contender2 + '</b>' + ' ' + $this.winBecause[i]);
       }
-
       $this.winOptions = options;
 
       return false;
@@ -40,10 +29,10 @@ let Sweepstake = function() {
 
     $this.addParticipant = function(name) {
         $this.participants.push(name);
-        $this.refresh();
+        return $this.participants;
     }
 
-    $this.generateOutput = function() {
+    $this.generateResults = function() {
         $this.setWinOptions();
 
         let container   = $this.containerOutput,
@@ -51,36 +40,19 @@ let Sweepstake = function() {
             winOptions  = $this.shuffle($this.winOptions),
             participantCount = $this.participants.length,
             participants = $this.participants,
-            output = [];
-
-       console.log('Participants', participants);
-       console.log('Participant count', participantCount);
-       console.log('Win options', winOptions);
-       console.log('Win option count', winOptionsCount);
+            results = new Array;
 
        let optionsPerParticipant = winOptionsCount / participantCount;
-
-       let index = 0,
-           outputHtml = '<h2>Sweepstake Results</h2>';
+       let index = 0;
 
        participants.forEach(function(Participant) {
-         outputHtml += '<p><b>' + Participant + '</b></p>';
-         outputHtml += '<ul>';
+         results[Participant] = new Array
          for(let i=0; i<optionsPerParticipant;i++) {
-           output.push({
-             Participant,
-             'winOption' : winOptions[index]
-           })
-           outputHtml += '<li>' + winOptions[index] + '</li>';
-
+           results[Participant].push(winOptions[index]);
            index++;
          }
-         outputHtml += '</ul>';
        });
-
-      $this.containerOutput.innerHTML = outputHtml;
-
-      return output;
+      return results;
     };
 
     $this.shuffle = function(array) {
@@ -89,61 +61,13 @@ let Sweepstake = function() {
           temp;
 
       while (i--) {
-
           j = Math.floor(Math.random() * (i+1));
-
           // swap randomly chosen element with current element
           temp = array[i];
           array[i] = array[j];
           array[j] = temp;
-
       }
 
       return array;
     }
-
-    $this.refresh = function() {
-        // Refresh participants div
-        // And clear the generated output
-        let participants = '<h2>Participants</h2>';
-          participants += '<ul>';
-        for(let i = 0; i<$this.participants.length; i++) {
-          participants += '<li>' + $this.participants[i] + '</li>';
-        }
-        participants += '</ul>';
-        $this.containerParticipants.innerHTML = participants;
-        $this.containerOutput.innerHTML = '';
-    }
-
-    $this.__construct = function() {
-      $this.container.appendChild($this.containerParticipants);
-      $this.container.appendChild($this.containerOutput);
-      $this.container.appendChild($this.generateBtn);
-      $this.generateBtn.addEventListener('click', $this.generateOutput);
-    }
-
-    $this.__construct();
 };
-
-let addParticipantLabel = document.createElement('LABEL');
-  addParticipantLabel.setAttribute('for', 'addParticipantInput');
-  addParticipantLabel.textContent = 'Add Participant';
-
-let addParticipantInput = document.createElement('INPUT');
-  addParticipantInput.setAttribute('type', 'text');
-  addParticipantInput.setAttribute('id', 'addParticipantInput');
-
-let addParticipantBtn = document.createElement('BUTTON');
-  addParticipantBtn.setAttribute('id', 'addParticipantBtn');
-  addParticipantBtn.textContent = 'Submit';
-
-let sweepstake = new Sweepstake();
-sweepstake.container.appendChild(addParticipantLabel);
-sweepstake.container.appendChild(addParticipantInput);
-sweepstake.container.appendChild(addParticipantBtn);
-
-addParticipantBtn.addEventListener('click', function(ev) {
-    let participantName = addParticipantInput.value;
-    addParticipantInput.value = '';
-    sweepstake.addParticipant(participantName);
-});
